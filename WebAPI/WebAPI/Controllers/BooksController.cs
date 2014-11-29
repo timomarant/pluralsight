86a5b76a54fc220a17f8549013ab091506283e58
timomarant <tmo@live.be>
2014-11-29 22:03:17 +0100

    commit

---
+++ WebAPI/WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,114 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class BooksController : ApiController
+    {
+        private WebAPIContext db = new WebAPIContext();
+
+        // GET: api/Book
+        public IQueryable<Book> GetBooks()
+        {
+            return db.Books;
+        }
+
+        // GET: api/Book/5
+        [ResponseType(typeof(Book))]
+        public IHttpActionResult GetBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // PUT: api/Book/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBook(int id, Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Book
+        [ResponseType(typeof(Book))]
+        public IHttpActionResult PostBook(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Books.Add(book);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
+        }
+
+        // DELETE: api/Book/5
+        [ResponseType(typeof(Book))]
+        public IHttpActionResult DeleteBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            db.Books.Remove(book);
+            db.SaveChanges();
+
+            return Ok(book);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BookExists(int id)
+        {
+            return db.Books.Count(e => e.Id == id) > 0;
+        }
+    }
+}
